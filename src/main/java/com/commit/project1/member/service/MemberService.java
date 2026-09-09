581dcdceb25bedea4bb65026b7c1835b2434c74d
rabo1226 <bohyun0330@gmail.com>
2026-09-09 17:27:08 +0900

    로그인 기능 member□dto,mapper,service,controller)생성

---
+++ src/main/java/com/commit/project1/member/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.commit.project1.member.service;
+
+import com.commit.project1.member.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+  private final MemberMapper memberMapper;
+
+}
