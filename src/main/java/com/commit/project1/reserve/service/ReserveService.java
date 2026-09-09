89f0cccf2bf5423f1787bc0e28e6c5f24ea27a06
jinho333 <wlsgho33@gmail.com>
2026-09-09 15:29:19 +0900

    예약 기본 컨í트롤러 구현

--- src/main/java/com/commit/project1/reserve/service/ReserveService.java
+++ src/main/java/com/commit/project1/reserve/service/ReserveService.java
@@ -1,4 +1,16 @@
 package com.commit.project1.reserve.service;
 
+import com.commit.project1.reserve.mapper.ReserveMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReserveService {
+  private final ReserveMapper reserveMapper;
+
+
+  //예약 정보 입력 관련 메서드
+
+  //예약일정 관련 메서드
 }
