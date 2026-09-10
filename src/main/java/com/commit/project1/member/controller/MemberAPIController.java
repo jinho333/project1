251b6b546ec3d6c5cb95a8a018ad866410119301
rabo1226 <bohyun0330@gmail.com>
2026-09-10 15:28:18 +0900

    로그인 기능 후 ì  구현

--- src/main/java/com/commit/project1/member/controller/MemberAPIController.java
+++ src/main/java/com/commit/project1/member/controller/MemberAPIController.java
@@ -7,26 +7,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
-@Controller
 @RequestMapping("/member-api")
+@RequiredArgsConstructor
+@RestController
 public class MemberAPIController {
   private final MemberService memberService;
 
   //로그인 버튼 클릭시 로그인 성공 여부 확인!(비동기 방식) -> 중복 확인 결과 !null면 로그인 성공!
   @GetMapping("/login")
-  public MemberDTO memberLogin(MemberDTO memberDTO, HttpServletRequest request){
+  public MemberDTO Login(MemberDTO memberDTO, HttpServletRequest request){
+    //로그인 조회!
     MemberDTO loginInfo = memberService.memberLoginCheck(memberDTO);
+    System.out.println(loginInfo);
     if (loginInfo != null){
       HttpSession session = request.getSession();
       session.setAttribute("loginInfo", loginInfo);
     }
     return loginInfo;
   }
+
+
+
+
 
 }
