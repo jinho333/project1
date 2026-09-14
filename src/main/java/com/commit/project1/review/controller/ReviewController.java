7d357b86d8a1be5b1eca5f01b4a77763b6c09b6c
jinho333 <wlsgho33@gmail.com>
2026-09-14 11:22:44 +0900

    리뷰 작성 디자인

---
+++ src/main/java/com/commit/project1/review/controller/ReviewController.java
@@ -0,0 +1,17 @@
+package com.commit.project1.review.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/review")
+public class ReviewController {
+  @GetMapping("/form")
+  public String reviewForm(){
+    return "pages/review/review_form";
+  }
+
+}
